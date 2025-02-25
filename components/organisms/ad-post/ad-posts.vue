289aadf41e99523f9a8d0eeb378568b0a5cd7a4c
alejandroyunes <alejandroyunes@outlook.com>
2025-02-25 15:55:55 -0500

    fix

--- components/organisms/ad-post/ad-posts.vue
+++ components/organisms/ad-post/ad-posts.vue
@@ -42,7 +42,6 @@ import GoBack from './go-back.vue';
           <ServicesSvg />
         </div>
       </div>
-      <h1>hola</h1>
     </div>
 
   </section>
@@ -51,56 +50,56 @@ import GoBack from './go-back.vue';
 <style lang="scss" scoped>
 .post {
   display: grid;
-  // grid-template-columns: repeat(4, 1fr);
-  // margin: 20px auto 0;
-  // gap: 20px;
-  // max-width: var(--max-width);
+  grid-template-columns: repeat(4, 1fr);
+  margin: 20px auto 0;
+  gap: 20px;
+  max-width: var(--max-width);
 
-  // @media (max-width: 1024px) {
-  //   grid-template-columns: repeat(3, 1fr);
-  //   gap: 16px;
-  // }
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 16px;
+  }
 
-  // @media (max-width: 768px) {
-  //   grid-template-columns: repeat(2, 1fr);
-  // }
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-  // .item {
-  //   display: flex;
-  //   flex-direction: column;
-  //   align-items: center;
+  .item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-  //   p {
-  //     color: var(--text);
-  //     font-size: 1.125rem;
-  //     font-weight: 600;
-  //   }
-  // }
+    p {
+      color: var(--text);
+      font-size: 1.125rem;
+      font-weight: 600;
+    }
+  }
 
-  // .ad-post-svg {
-  //   width: 180px;
-  //   height: 150px;
-  //   display: flex;
-  //   flex-direction: column;
-  //   align-items: center;
-  //   justify-content: center;
-  //   margin-top: 20px;
-  //   border: 1px solid var(--primary);
-  //   cursor: pointer;
+  .ad-post-svg {
+    width: 180px;
+    height: 150px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    border: 1px solid var(--primary);
+    cursor: pointer;
 
-  //   &:hover {
-  //     background-color: var(--primary);
-  //   }
+    &:hover {
+      background-color: var(--primary);
+    }
 
-  //   @media (max-width: 768px) {
-  //     width: 140px;
-  //     height: 120px;
-  //   }
+    @media (max-width: 768px) {
+      width: 140px;
+      height: 120px;
+    }
 
-  //   @media (max-width: 480px) {
-  //     width: 120px;
-  //     height: 100px;
-  //   }
-  // }
+    @media (max-width: 480px) {
+      width: 120px;
+      height: 100px;
+    }
+  }
 }
 </style>
